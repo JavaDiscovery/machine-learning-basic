c22e56095cc8ecd7ef980ad70e8125ee7a333662
zhengxiongfeng <zhengxiongfeng@baidu.com>
2019-01-09 21:23:35 +0800

    基于TensorFlow Estimator实现tf-idf表示的文本简单分类模型

--- tensorflow_nlp/tfidf_text_classification.py
+++ tensorflow_nlp/tfidf_text_classification.py
@@ -21,13 +21,63 @@ def sparse_tfidf(x):
     for i, indices in enumerate(x):
         for idx in indices:
             count[i, idx] += 1
-    print "%.2f secs ==> Document-Term Matrix.\n" % (time.time() - t0)
+    print ("%.2f secs ==> Document-Term Matrix.\n" % (time.time() - t0))
 
     t0 = time.time()
     x= TfidfTransformer.fit_transform(count)
-    print "%.2f secs ==> TF-IDF transform.\n" % (time.time() - t0)
+    print ("%.2f secs ==> TF-IDF transform.\n" % (time.time() - t0))
     return x
 
 def next_train_batch(x, y):
     for i in range(0, x.shape[0], BATCH_SIZE):
         yield x[i: i+BATCH_SIZE].toarray(), y[i: i+BATCH_SIZE]
+
+def next_test_batch(x):
+    for i in range(0, x.shape[0], BATCH_SIZE):
+        yield x[i: i+BATCH_SIZE].toarray()
+
+def train_input_fn(x_train, y_train):
+    dataset = tf.data.Dataset.from_generator(
+        lambda: next_train_batch(x_train, y_train), (tf.float32, tf.int64),
+        (tf.TensorShape([None, VOCAB_SIZE]), tf.TensorShape([None]))
+    )
+    iterator = dataset.make_one_shot_iterator()
+    return iterator.get_next()
+
+def predict_input_fn(x_test):
+    dataset = tf.data.Dataset.from_generator(
+        lambda : next_test_batch(x_test), tf.float32,
+        tf.TensorShape([None, VOCAB_SIZE])
+    )
+    iterator = dataset.make_one_shot_iterator()
+    return iterator.get_next()
+
+def model_fn(features, labels, mode, params):
+    logits = tf.layers.dense(features, N_CLASS)
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode, predictions=tf.argmax(logits, -1))
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        loss_op = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+            logits=logits, labels=labels
+        ))
+        train_op = tf.train.AdamOptimizer(LR).minimize(loss_op,
+            global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss_op, train_op=train_op)
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.imdb.load_data(num_words=VOCAB_SIZE)
+x_train = sparse_tfidf(x_train)
+x_test = sparse_tfidf(x_test)
+
+estimator = tf.estimator.Estimator(model_fn)
+
+for _ in range(N_EPOCH):
+    estimator.tf.keras.datasets.imdb.load_data(num_words=VOCAB_SIZE)
+    x_train = sparse_tfidf(x_train)
+    x_test = sparse_tfidf(x_test)
+
+    estimator = tf.estimator.Estimator(model_fn)
+
+    for _ in range(N_EPOCH):
+        estimator.train(lambda : train_input_fn(*shuffle(x_train, y_train)))
+        y_pred = np.fromiter(estimator.predict(lambda : predict_input_fn(x_test)), np.int32)
+        print ("\nValidation Accuracy: %.4f" % (y_pred==y_test).mean())
